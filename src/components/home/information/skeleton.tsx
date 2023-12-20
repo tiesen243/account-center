7d9ccd1273d63cf6d161ecc6de17825db6b08ec2
Tiesen <ttien56906@gmail.com>
2023-12-20 15:07:14 +0700

    fix skeleton loading

--- src/components/home/information/skeleton.tsx
+++ src/components/home/information/skeleton.tsx
@@ -5,15 +5,15 @@ const InfoSkeleton = () => (
   <HomeLayout
     title="Account Information"
     description="Baisc infomation, like your name and photo"
-    className="grid grid-cols-1 items-center md:grid-cols-3"
+    className="grid grid-cols-1 items-center gap-0 md:grid-cols-3 md:gap-2"
   >
     <article className="col-span-2">
       {Array.from({ length: 6 }).map((_, i) => (
-        <Skeleton key={i} className="my-4 h-4 w-1/2" />
+        <Skeleton key={i} className="my-4 h-4 w-4/5" />
       ))}
     </article>
 
-    <Skeleton className="aspect-square w-full place-self-center rounded object-cover" />
+    <Skeleton className="row-start-1 my-4 aspect-square w-full place-self-center rounded object-cover shadow-lg md:col-start-3" />
   </HomeLayout>
 )
 
